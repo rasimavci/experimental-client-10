d3c0dc53005280d337af3f87bc95a48a3e81dc2f
ASUS-MAIKI\maiki <maikinahara@gmail.com>
2018-04-20 11:54:46 -0300

    change navbar style

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -2,11 +2,11 @@
 f7-page
   f7-navbar(link='/about/', title='ravci')
     i.icon.material-icons.md-only account_box
-  f7-list
-    f7-list-item(link='/about/', title='About', view='#main-view', panel-close='')
+  f7-list#navList
+    f7-list-item.no-icon(link='/about/', title='About', view='#main-view', panel-close='')
     f7-list-item(link='/form/', title='Form', view='#main-view', panel-close='')
     f7-list-item(link='/call/', title='Call', view='#main-view', panel-close='')
-      i.icon.material-icons.md-only(align="left") phone
+      i.icon.material-icons.md-only.test-icon-left phone
     f7-list-item(link='/presences/', title='Presences', view='#main-view', panel-close='')
     f7-list-item(link='/favorites/', title='Favorites', view='#main-view', panel-close='')
      i.icon.material-icons.md-only star_border
@@ -15,7 +15,7 @@ f7-page
     f7-list-item(link='/history/', title='History', view='#main-view', panel-close='')
      i.icon.material-icons.md-only access_time
     f7-list-item(badge="3" link='/message/', title='Inbox', view='#main-view', panel-close='')
-     i.icon.material-icons.md-only inbox
+     i.icon.material-icons.md-only.test-icon-left inbox
     f7-list-item(link='/dialpad/', title='Dialpad', view='#main-view', panel-close='')
      i.icon.material-icons.md-only inbox
     f7-list-item(link='/sessions/', title='Sessions', view='#main-view', panel-close='')
