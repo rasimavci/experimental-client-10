35742b9f0c48443d5ce5230dde645db816071a0a
ASUS-MAIKI\maiki <maikinahara@gmail.com>
2018-05-09 11:17:31 -0300

    fix button, and explanation in class

--- src/pages/conference.vue
+++ src/pages/conference.vue
@@ -5,7 +5,7 @@ f7-page
       f7-link(icon-if-ios='f7:menu', icon-if-md='material:menu', panel-open='left')
     f7-nav-title Collaboration
   .button-containerX
-    f7-button.col(raised='', fill='') START COLLABORATION
+    f7-button.col.this-is-why-i-use-f7-components-and-not-f7vuecomponents-f7vuecomponents-are-created-AFTER-render(raised='', fill='') START COLLABORATION
     //-f7-button.col(big round open-popup='#popup') START COLLABORATION
   f7-block(strong='')
     p You can also send an automated invitation
